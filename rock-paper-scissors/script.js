795ccca1fb4d8e4beb79f0edc343663e58c8b6a3
Ian Williams <59708779+iwill434@users.noreply.github.com>
2025-09-19 14:22:08 -0400

    kind of went crazy on this and did it all at once

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,58 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame(){
+
+    function getComputerChoice() {
+        let compChoice = Math.random();
+        if (compChoice <= 0.33) {
+            return "Rock";
+        } else if (compChoice <= 0.67) {
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    }
+
+    let getHumanChoice = prompt("Please enter your input for rock paper scissors: ");
+
+    let humanChoice = getHumanChoice;
+    let computerChoice = getComputerChoice();
+
+    function playRound(humanChoice, computerChoice) {
+        let human = humanChoice.toLowerCase();
+        let computer = computerChoice.toLowerCase();
+        if (human === computer) {
+            console.log(`Draw. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else if (human === "rock" && computer === "scissors") {
+            updateHumanScore.textContent = ++humanScore;
+            console.log(`Rock beats scissors. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else if (human === "rock" && computer === "paper") {
+            updateComputerScore.textContent = ++computerScore;
+            console.log(`Rock loses to paper. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else if (human === "paper" && computer === "scissors") {
+            updateComputerScore.textContent = ++computerScore;
+            console.log(`Paper loses to scissors. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else if (human === "paper" && computer === "rock") {
+            updateHumanScore.textContent = ++humanScore;
+            console.log(`Paper beats rock. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else if (human === "scissors" && computer === "rock") {
+            updateComputerScore.textContent = ++computerScore;
+            console.log(`Scissors loses to rock. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else if (human === "scissors" && computer === "paper") {
+            updateHumanScore.textContent = ++humanScore;
+            console.log(`Scissors beat paper. The current score is Human: ${humanScore} Computer: ${computerScore}`);
+        } else {
+            console.log('Please input either "rock", "paper", or "scissors"');
+        }
+    }
+
+    playRound(humanChoice, computerChoice);
+}
+
+function playAgain(){
+    playGame();
+}
+
+let updateHumanScore = document.getElementById("humanScore");
+let updateComputerScore = document.getElementById("computerScore");
