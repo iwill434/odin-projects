d49b1e605d4d799a32454c8d50eedde77f1734b4
Ian Williams <59708779+iwill434@users.noreply.github.com>
2025-10-02 14:13:24 -0400

    input functionality

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -8,30 +8,30 @@ function changeNumColumns () {
 
 }
 
+function createGrid() {
+    for (i = 0; i < numColumns; i++) {
+        const makeColumn = document.createElement("div");
+        makeColumn.classList.add("column");
+        container.appendChild(makeColumn);
 
-for (i = 0; i < numColumns; i++) {
-    const makeColumn = document.createElement("div");
-    makeColumn.classList.add("column");
-    container.appendChild(makeColumn);
+        if (numRows > 1) {
+            for (j = 0; j < numRows; j++) {
+                const makeRow = document.createElement("div");
+                makeRow.classList.add("row");
+                makeColumn.appendChild(makeRow);
 
-    if (numRows > 1) {
-        for (j = 0; j < numRows; j++) {
-            const makeRow = document.createElement("div");
-            makeRow.classList.add("row");
-            makeColumn.appendChild(makeRow);
+                // Moving hover effect into function
+                makeRow.addEventListener("mouseover", changeColor);
+            }
         }
     }
+    // Update the boxes after creating the grid
+    boxes = document.querySelectorAll(".row");
 }
 
 // Hover effect
 function changeColor() {
     this.style.backgroundColor = "black";
-}
-
-const boxes = document.querySelectorAll(".row");
-
-for (let i = 0; i < boxes.length; i++){
-    boxes[i].addEventListener("mouseover",changeColor);
 }
 
 // Button functionality
@@ -44,3 +44,23 @@ function resetColor() {
 }
 
 resetButton.addEventListener("click", resetColor);
+
+const sizeButton = document.getElementById("size-button");
+
+function setGridSize() {
+    const columnInput = document.getElementById("column-input");
+    const rowInput = document.getElementById("row-input");
+    numColumns = parseInt(columnInput.value);
+    numRows = parseInt(rowInput.value);
+
+    container.innerHTML = "";
+
+    createGrid();
+
+    document.getElementById("updatedColumns").textContent = `${numColumns} columns`;
+    document.getElementById("updatedRows").textContent = `${numRows} rows`;
+}
+
+sizeButton.addEventListener("click", setGridSize)
+
+createGrid();
